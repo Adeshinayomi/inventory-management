0484d1870426a090d4c0cf3bf739338b880db096
Adeshinayomi <adeshinabhadmus5@gmail.com>
2026-08-23 16:36:14 +0100

    installed dotenv for environmental variables

--- Backend/app.js
+++ Backend/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
 
 app.use(express.json());
 
 
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
