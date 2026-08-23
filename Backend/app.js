437055579c92d2f1e13a6aa7172eb397c4ff248a
Adeshinayomi <adeshinabhadmus5@gmail.com>
2026-08-23 16:43:50 +0100

    Installed mongoose and connected mongoDB creating a config file

--- Backend/app.js
+++ Backend/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const connectDB = require("./Config/DatabaseConfig");
 const dotenv = require("dotenv");
 dotenv.config();
 
@@ -8,4 +9,5 @@ app.use(express.json());
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
+  connectDB();
 });
