75d6570487c8326f56265ca3fba6ebe08f52adcc
Adeshinayomi <adeshinabhadmus5@gmail.com>
2026-08-24 12:36:38 +0100

    added role based authorization for product creation

--- Backend/Routes/ProductRoute.js
+++ Backend/Routes/ProductRoute.js
@@ -3,8 +3,9 @@ const route = require("express").Router();
 const ProductController = require("../Controller/ProductController.js");
 const authenticateToken= require("../Middleware/Auth.js");
 const upload = require("../Middleware/uploadIMage.js");
+const authorize= require("../Middleware/role.js").authorize;
 
-route.post("/create-product", authenticateToken, upload.single("image"), ProductController.createProduct);
+route.post("/create-product", authenticateToken,authorize(["owner"]), upload.single("image"), ProductController.createProduct);
 
 
 module.exports = route;
