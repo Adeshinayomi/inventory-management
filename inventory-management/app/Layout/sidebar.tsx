de7ab207f18f6248ac08f8f8592e99327b1b7784
Adeshinayomi <adeshinabhadmus5@gmail.com>
2026-08-25 22:06:04 +0100

    changed the size of text on sidebar

--- inventory-management/app/Layout/sidebar.tsx
+++ inventory-management/app/Layout/sidebar.tsx
@@ -4,42 +4,42 @@ export function Sidebar(){
     return(
         <nav className="w-1/5 min-h-full fixed bg-sidebar p-5 grid gap-10 text-text-on-primary content-between">
             <div className="grid gap-10">
-                <h1 className="text-2xl font-medium">
+                <h1 className="text-xl font-medium">
                     <span className="text-primary">IN</span>VENTORY
                 </h1>
 
-                <ul className="w-5/6 grid gap-3 self-stretch h-full">
+                <ul className="w-5/6 grid gap-3 self-stretch h-full text-sm">
                     <li className="flex gap-2 bg-sidebar-active px-3 py-2 rounded-md">
-                        <LayoutDashboard className="text-sidebar-text-active"/>
-                        <span>Dashboard</span>
+                        <LayoutDashboard size={20} className="text-sidebar-text-active"/>
+                        <span className="self-center">Dashboard</span>
                     </li>
 
                     <li className="flex gap-2 px-3 py-2 rounded-md">
-                        <Warehouse/>
-                        <span>Inventory</span>
+                        <Warehouse size={20}/>
+                        <span className="self-center">Inventory</span>
                     </li>
 
                     <li className="flex gap-2 px-3 py-2 rounded-md">
-                        <Box/>
-                        <span>Products</span>
+                        <Box size={20}/>
+                        <span className="self-center">Products</span>
                     </li>
 
                     <li className="flex gap-2 px-3 py-2 rounded-md">
-                        <ChartNoAxesCombined/>
-                        <span>Products</span>
+                        <ChartNoAxesCombined size={20}/>
+                        <span className="self-center">Products</span>
                     </li>
                 </ul>
             </div>
 
 
-            <ul>
+            <ul className="text-sm">
                 <li className="flex gap-2 px-3 py-2 rounded-md">
-                    <Settings/>
+                    <Settings size={20}/>
                     <span>Settings</span>
                 </li>
 
                 <li className="flex gap-2 px-3 py-2 rounded-md">
-                    <LogOut/>
+                    <LogOut size={20}/>
                     <span>Logout</span>
                 </li>
             </ul>
