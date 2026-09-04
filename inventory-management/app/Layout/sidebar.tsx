54f2a750a3dd0bc0924e61cf5a2a999445998ced
Adeshinayomi <adeshinabhadmus5@gmail.com>
2026-09-04 16:25:51 +0100

    enhance sidebar navigation with links and active state styling

--- inventory-management/app/Layout/sidebar.tsx
+++ inventory-management/app/Layout/sidebar.tsx
@@ -1,46 +1,49 @@
+"use client"
 import { LayoutDashboard,Warehouse,Box,ChartNoAxesCombined,Settings,LogOut} from "lucide-react"
-
+import Link from "next/link"
+import  { usePathname } from "next/navigation"
 export function Sidebar(){
+    const pathname = usePathname()
     return(
         <nav className="w-1/5 fixed min-h-screen bg-sidebar p-5 grid gap-10 text-text-on-primary content-between">
             <div className="grid gap-10">
                 <h1 className="text-xl font-medium">
-                    <span className="text-primary">IN</span>VENTORY
+                    <Link href="/" className="text-primary">IN</Link>VENTORY
                 </h1>
 
                 <ul className="w-5/6 grid gap-3 self-stretch h-full text-sm">
-                    <li className="flex gap-2 bg-sidebar-active px-3 py-2 rounded-md">
-                        <LayoutDashboard size={20} className="text-sidebar-text-active"/>
-                        <span className="self-center">Dashboard</span>
+                    <li className={`flex gap-2 px-3 py-2 rounded-md ${pathname === "/" ? "bg-sidebar-active text-sidebar-text-active" : ""}`}>
+                        <LayoutDashboard size={20} />
+                        <Link href="/" className="self-center text-white ">Dashboard</Link>
                     </li>
 
-                    <li className="flex gap-2 px-3 py-2 rounded-md">
+                    <li className={`flex gap-2 px-3 py-2 rounded-md ${pathname === "/inventory" ? "bg-sidebar-active text-sidebar-text-active" : ""}`}>
                         <Warehouse size={20}/>
-                        <span className="self-center">Inventory</span>
+                        <Link href="/inventory" className="self-center text-white">Inventory</Link>
                     </li>
 
-                    <li className="flex gap-2 px-3 py-2 rounded-md">
+                    <li className={`flex gap-2 px-3 py-2 rounded-md ${pathname === "/products" ? "bg-sidebar-active text-sidebar-text-active" : ""}`}>
                         <Box size={20}/>
-                        <span className="self-center">Products</span>
+                        <Link href="/products" className="self-center text-white">Products</Link>
                     </li>
 
-                    <li className="flex gap-2 px-3 py-2 rounded-md">
+                    <li className={`flex gap-2 px-3 py-2 rounded-md ${pathname === "/sales" ? "bg-sidebar-active text-sidebar-text-active" : ""}`}>
                         <ChartNoAxesCombined size={20}/>
-                        <span className="self-center">Products</span>
+                        <Link href="/sales" className="self-center text-white">Sales</Link>
                     </li>
                 </ul>
             </div>
 
 
             <ul className="text-sm">
-                <li className="flex gap-2 px-3 py-2 rounded-md">
+                <li className={`flex gap-2 px-3 py-2 rounded-md ${pathname === "/settings" ? "bg-sidebar-active text-sidebar-text-active" : ""}`}>
                     <Settings size={20}/>
-                    <span>Settings</span>
+                    <Link href="/settings" className="self-center text-white">Settings</Link>
                 </li>
 
-                <li className="flex gap-2 px-3 py-2 rounded-md">
+                <li className={`flex gap-2 px-3 py-2 rounded-md ${pathname === "/logout" ? "bg-sidebar-active text-sidebar-text-active" : ""}`}>
                     <LogOut size={20}/>
-                    <span>Logout</span>
+                    <Link href="/logout" className="self-center text-white">Logout</Link>
                 </li>
             </ul>
         </nav>
