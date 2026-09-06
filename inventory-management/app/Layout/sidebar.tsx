62dc6bf8aba64f0d8b45660cac5113b25851f7d9
Adeshinayomi <adeshinabhadmus5@gmail.com>
2026-09-06 13:54:09 +0100

    refactor LogOut component: move to separate file and update sidebar integration

--- inventory-management/app/Layout/sidebar.tsx
+++ inventory-management/app/Layout/sidebar.tsx
@@ -1,51 +1,61 @@
 "use client"
-import { LayoutDashboard,Warehouse,Box,ChartNoAxesCombined,Settings,LogOut} from "lucide-react"
+import { LayoutDashboard,Warehouse,Box,ChartNoAxesCombined,Settings,LogOutIcon} from "lucide-react"
 import Link from "next/link"
+import { LogOut } from "../component/LogOut"
 import  { usePathname } from "next/navigation"
+import { useState } from "react"
 export function Sidebar(){
     const pathname = usePathname()
+    const [isModalOpen, setIsModalOpen] = useState(false)
     return(
-        <nav className="w-1/5 fixed min-h-screen bg-sidebar p-5 grid gap-10 text-text-on-primary content-between">
-            <div className="grid gap-10">
-                <h1 className="text-xl font-medium">
-                    <Link href="/" className="text-primary">IN</Link>VENTORY
-                </h1>
+        <>
+            <nav className="w-1/5 fixed min-h-screen bg-sidebar p-5 grid gap-10 text-text-on-primary content-between">
+                <div className="grid gap-10">
+                    <h1 className="text-xl font-medium">
+                        <Link href="/" className="text-primary">IN</Link>VENTORY
+                    </h1>
 
-                <ul className="w-5/6 grid gap-3 self-stretch h-full text-sm">
-                    <li className={`flex gap-2 px-3 py-2 rounded-md ${pathname === "/" ? "bg-sidebar-active text-sidebar-text-active" : ""}`}>
-                        <LayoutDashboard size={20} />
-                        <Link href="/" className="self-center text-white ">Dashboard</Link>
+                    <ul className="w-5/6 grid gap-3 self-stretch h-full text-sm">
+                        <li className={`flex gap-2 px-3 py-2 rounded-md ${pathname === "/" ? "bg-sidebar-active text-sidebar-text-active" : ""}`}>
+                            <LayoutDashboard size={20} />
+                            <Link href="/" className="self-center text-white ">Dashboard</Link>
+                        </li>
+
+                        <li className={`flex gap-2 px-3 py-2 rounded-md ${pathname === "/inventory" ? "bg-sidebar-active text-sidebar-text-active" : ""}`}>
+                            <Warehouse size={20}/>
+                            <Link href="/inventory" className="self-center text-white">Inventory</Link>
+                        </li>
+
+                        <li className={`flex gap-2 px-3 py-2 rounded-md ${pathname === "/product" ? "bg-sidebar-active text-sidebar-text-active" : ""}`}>
+                            <Box size={20}/>
+                            <Link href="/product" className="self-center text-white">Products</Link>
+                        </li>
+
+                        <li className={`flex gap-2 px-3 py-2 rounded-md ${pathname === "/sales" ? "bg-sidebar-active text-sidebar-text-active" : ""}`}>
+                            <ChartNoAxesCombined size={20}/>
+                            <Link href="/sales" className="self-center text-white">Sales</Link>
+                        </li>
+                    </ul>
+                </div>
+
+
+                <ul className="text-sm">
+                    <li className={`flex gap-2 px-3 py-2 rounded-md ${pathname === "/settings" ? "bg-sidebar-active text-sidebar-text-active" : ""}`}>
+                        <Settings size={20}/>
+                        <Link href="/settings" className="self-center text-white">Settings</Link>
                     </li>
 
-                    <li className={`flex gap-2 px-3 py-2 rounded-md ${pathname === "/inventory" ? "bg-sidebar-active text-sidebar-text-active" : ""}`}>
-                        <Warehouse size={20}/>
-                        <Link href="/inventory" className="self-center text-white">Inventory</Link>
-                    </li>
-
-                    <li className={`flex gap-2 px-3 py-2 rounded-md ${pathname === "/product" ? "bg-sidebar-active text-sidebar-text-active" : ""}`}>
-                        <Box size={20}/>
-                        <Link href="/product" className="self-center text-white">Products</Link>
-                    </li>
-
-                    <li className={`flex gap-2 px-3 py-2 rounded-md ${pathname === "/sales" ? "bg-sidebar-active text-sidebar-text-active" : ""}`}>
-                        <ChartNoAxesCombined size={20}/>
-                        <Link href="/sales" className="self-center text-white">Sales</Link>
+                    <li className='flex gap-2 px-3 py-2 rounded-md' onClick={
+                        ()=>{
+                            setIsModalOpen((prev)=>!prev)
+                        }
+                    }>
+                        <LogOutIcon size={20}/>
+                        <span className="self-center text-white">Logout</span>
                     </li>
                 </ul>
-            </div>
-
-
-            <ul className="text-sm">
-                <li className={`flex gap-2 px-3 py-2 rounded-md ${pathname === "/settings" ? "bg-sidebar-active text-sidebar-text-active" : ""}`}>
-                    <Settings size={20}/>
-                    <Link href="/settings" className="self-center text-white">Settings</Link>
-                </li>
-
-                <li className='flex gap-2 px-3 py-2 rounded-md'>
-                    <LogOut size={20}/>
-                    <span className="self-center text-white">Logout</span>
-                </li>
-            </ul>
-        </nav>
+            </nav>
+            <LogOut isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen}/>
+        </>
     )
 }
--- inventory-management/app/layout.tsx
+++ inventory-management/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter, Geist } from "next/font/google";
 import { Sidebar } from "./Layout/sidebar";
 import { Header } from "./Layout/header";
-import { LogOut } from "./Layout/LogOut";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 
@@ -32,8 +31,6 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
 
           <section className="grid w-4/5 ml-auto">
             <Header />
-            <LogOut />
-
             {children}
           </section>
 
